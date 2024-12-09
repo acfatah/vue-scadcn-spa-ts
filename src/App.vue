b24af3a467bdae95f0c473a410770eb889129e0f
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-10 00:35:11 +0800

    chore: lint fix

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="flex justify-center items-center gap-4 min-h-screen flex-col">
-    <div class="flex [&_img]:size-24 gap-4">
+  <div class="flex min-h-screen flex-col items-center justify-center gap-4">
+    <div class="flex gap-4 [&_img]:size-24">
       <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" alt="Vite logo" />
+        <img src="/vite.svg" alt="Vite logo">
       </a>
       <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" alt="Vue logo" />
+        <img src="./assets/vue.svg" alt="Vue logo">
       </a>
     </div>
-    <h1 class="text-3xl font-bold">Hello Vite and Vue!</h1>
+    <h1 class="text-3xl font-bold">
+      Hello Vite and Vue!
+    </h1>
   </div>
 </template>
