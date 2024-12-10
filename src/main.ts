fbd59615bb84d61b278e0f48c8ce26133a3e8fc3
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-11 04:51:09 +0800

    Add Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/app.css'
 
+document.title = 'Vue + Vite'
+
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 app.mount('#app')
