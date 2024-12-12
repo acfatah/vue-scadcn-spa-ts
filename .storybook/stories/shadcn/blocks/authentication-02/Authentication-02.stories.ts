a7f374039ed7086b68e32f5cb3cf220bf3348464
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-12 13:04:32 +0800

    refactor: Authentication-02

--- .storybook/stories/shadcn/blocks/authentication-02/Authentication-02.stories.ts
+++ .storybook/stories/shadcn/blocks/authentication-02/Authentication-02.stories.ts
@@ -1,7 +1,8 @@
 import { vueRouter } from 'storybook-vue3-router'
 import App from '~/App.vue'
+import { Toaster } from '~/components/ui/toast'
 import routes from './routes.ts'
-import DefaultSource from './views/LoginView.vue?raw'
+import DefaultSource from './views/Login/LoginView.vue?raw'
 
 export default {
   title: 'shadcn/Blocks/Authentication-02',
@@ -25,4 +26,9 @@ export const Default = {
       },
     },
   },
+
+  render: () => ({
+    components: { App, Toaster },
+    template: '<Toaster /><App />',
+  }),
 }
--- .storybook/stories/shadcn/blocks/authentication-02/routes.ts
+++ .storybook/stories/shadcn/blocks/authentication-02/routes.ts
@@ -1,11 +1,34 @@
+import type { RouteRecordRaw } from 'vue-router'
 import CenteredLayout from './layouts/CenteredLayout.vue'
-import LoginView from './views/LoginView.vue'
+import LoginView from './views/Login/LoginView.vue'
 
-export default [
+const routes: RouteRecordRaw[] = [
   {
     path: '/login',
     name: 'login',
     meta: { layout: CenteredLayout },
     component: LoginView,
   },
+
+  {
+    path: '/register',
+    name: 'register',
+    component: {
+      render() {
+        window.location.href = '/iframe.html?globals=&args=&id=shadcn-blocks-sign-up--default&viewMode=story'
+      },
+    },
+  },
+
+  {
+    path: '/reset-password',
+    name: 'reset-password',
+    component: {
+      render() {
+        window.location.href = '/iframe.html?globals=&args=&id=shadcn-blocks-reset-password--default&viewMode=story'
+      },
+    },
+  },
 ]
+
+export default routes
