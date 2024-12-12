218ef8f2992f0a34eacaa3ba9fcbf15ae670dd29
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-12 23:22:27 +0800

    fix: SidebarContent, add ScrollArea as auto by default

--- src/components/ui/scroll-area/index.ts
+++ src/components/ui/scroll-area/index.ts
@@ -1,2 +1,4 @@
 export { default as ScrollArea } from './ScrollArea.vue'
 export { default as ScrollBar } from './ScrollBar.vue'
+
+export type ScrollType = 'auto' | 'always' | 'scroll' | 'hover'
--- src/components/ui/sidebar/SidebarContent.vue
+++ src/components/ui/sidebar/SidebarContent.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
+import { ScrollArea, type ScrollType } from '~/components/ui/scroll-area'
 import { cn } from '~/lib/utils'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   class?: HTMLAttributes['class']
-}>()
+  scrollType?: ScrollType
+}>(), {
+  scrollType: 'auto',
+})
 </script>
 
 <template>
@@ -12,6 +16,8 @@ const props = defineProps<{
     data-sidebar="content"
     :class="cn('flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden', props.class)"
   >
-    <slot />
+    <ScrollArea :type="props.scrollType">
+      <slot />
+    </ScrollArea>
   </div>
 </template>
