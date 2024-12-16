e470fae80f695a3f68fcd8ed9fc232095153daf7
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-16 09:32:31 +0800

    feat(Label): add disabled state handling and forward props
    - Added `useForwardProps` to forward props to the `Label` component.
    - Introduced `disabled` prop and injected `disabled` state to handle disabled styling.
    - Updated class binding to include `disabled` state styles.

--- src/components/ui/label/Label.vue
+++ src/components/ui/label/Label.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
-import { Label, type LabelProps } from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
+import { Label, type LabelProps, useForwardProps } from 'radix-vue'
+import { computed, type HTMLAttributes, inject } from 'vue'
 import { cn } from '~/lib/utils'
 
-const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<LabelProps & {
+  class?: HTMLAttributes['class']
+  disabled?: boolean
+}>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
 
   return delegated
 })
+
+const forwardedProps = useForwardProps(delegatedProps)
+const disabled = inject('disabled', false)
 </script>
 
 <template>
   <Label
-    v-bind="delegatedProps"
+    v-bind="forwardedProps"
     :class="
       cn(
-        'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+        'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 disabled:cursor-not-allowed',
         props.class,
+        { 'opacity-70 cursor-not-allowed': props.disabled || disabled },
       )
     "
   >
