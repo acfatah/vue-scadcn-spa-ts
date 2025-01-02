3e1b8bc58cfa9bb1a1af61277b6f6b132ed6e4f4
Achmad F. Ibrahim <acfatah@gmail.com>
2025-01-03 04:06:07 +0800

    chore: disable Storybook telemetry

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,10 @@
 import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
+  core: {
+    // https://storybook.js.org/docs/configure/telemetry
+    disableTelemetry: true,
+  },
   stories: [
     './stories/**/*.mdx',
     './stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
