299448aad3fd61620955c9b9299d996f9841ffad
Achmad F. Ibrahim <acfatah@gmail.com>
2025-01-06 05:29:36 +0800

    feat(storybook): Add Alert component stories with default and destructive variants
    - Introduce Storybook stories for the Alert component, including the default and destructive variations.
    - Implement DefaultStory.vue to demonstrate the standard usage of the Alert component.
    - Create DestructiveAlert.vue for showcasing a variant of Alert styled to indicate error or warning messages.

---
+++ .storybook/stories/ui/Alert/Alert.stories.ts
@@ -0,0 +1,68 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from '~/components/ui/alert'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+import DestructiveAlertStory from './DestructiveAlert.vue'
+import DestructiveAlertSource from './DestructiveAlert.vue?raw'
+
+/**
+ * Displays a callout for user attention.
+ */
+export default {
+  title: 'Panels/Alert',
+  component: DefaultStory,
+  subcomponents: {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
+
+export const Destructive: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DestructiveAlertSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DestructiveAlertStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DestructiveAlertStory v-bind="args" />
+    `,
+  }),
+}
