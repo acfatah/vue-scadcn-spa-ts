d417780e9908d802f7cd4999ef10893c139d847f
Achmad F. Ibrahim <acfatah@gmail.com>
2025-01-26 22:22:48 +0800

    feat(storybook): Add Card component stories

---
+++ .storybook/stories/ui/Card/Card.stories.ts
@@ -0,0 +1,75 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '~/components/ui/card'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+import NotificationExampleComponent from './NotificationExample.vue'
+import NotificationExampleSource from './NotificationExample.vue?raw'
+
+/**
+ * Displays a card with header, content, and footer.
+ */
+export default {
+  title: 'Panels/Card',
+  component: Card,
+  subcomponents: {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
+
+export const NotificationExample: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: NotificationExampleSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { NotificationExampleComponent },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <NotificationExampleComponent v-bind="args" />
+    `,
+  }),
+}
