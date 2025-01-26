f4347b86669ed353707019eca4166d871b87e1c1
Achmad F. Ibrahim <acfatah@gmail.com>
2025-01-26 22:37:05 +0800

    feat(storybook): Add Collapsible component stories

---
+++ .storybook/stories/ui/Collapsible/Collapsible.stories.ts
@@ -0,0 +1,46 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '~/components/ui/collapsible'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * An interactive component which expands/collapses a panel.
+ *
+ * Primitive API Reference: https://www.radix-vue.com/components/collapsible.html
+ */
+export default {
+  title: 'Panels/Collapsible',
+  component: DefaultStory,
+  subcomponents: {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
