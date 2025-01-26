b4843638227a50448e9ede4b8099c8c45cf66444
Achmad F. Ibrahim <acfatah@gmail.com>
2025-01-27 07:13:07 +0800

    feat(storybook): Add Dialog component stories

---
+++ .storybook/stories/ui/Dialog/Dialog.stories.ts
@@ -0,0 +1,185 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogScrollContent,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog'
+import { Toaster } from '~/components/ui/toast'
+import CustomCloseButtonComponent from './CustomCloseButton.vue'
+import CustomCloseButtonSource from './CustomCloseButton.vue?raw'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+import DialogWithContextMenuTriggerComponent from './DialogWithContextMenuTrigger.vue'
+import DialogWithContextMenuTriggerSource from './DialogWithContextMenuTrigger.vue?raw'
+import FormDialogComponent from './FormDialog.vue'
+import FormDialogSource from './FormDialog.vue?raw'
+import ScrollBodyComponent from './ScrollBody.vue'
+import ScrollBodySource from './ScrollBody.vue?raw'
+import ScrollOverlayComponent from './ScrollOverlay.vue'
+import ScrollOverlaySource from './ScrollOverlay.vue?raw'
+
+/**
+ * A modal dialog that interrupts the user with important content and expects a response.
+ */
+export default {
+  title: 'Overlays/Dialog',
+  component: DefaultStory,
+  subcomponents: {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogScrollContent,
+    DialogTitle,
+    DialogTrigger,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
+
+export const CustomCloseButton: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: CustomCloseButtonSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { CustomCloseButtonComponent },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <CustomCloseButtonComponent v-bind="args" />
+    `,
+  }),
+}
+
+export const ScrollBody: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: ScrollBodySource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { ScrollBodyComponent },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <ScrollBodyComponent v-bind="args" />
+    `,
+  }),
+}
+
+export const ScrollOverlay: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: ScrollOverlaySource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { ScrollOverlayComponent },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <ScrollOverlayComponent v-bind="args" />
+    `,
+  }),
+}
+
+export const Form: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: FormDialogSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { FormDialogComponent, Toaster },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <Teleport to="body">
+        <Toaster />
+      </Teleport>
+      <FormDialogComponent v-bind="args" />
+    `,
+  }),
+}
+
+/**
+ * To activate the Dialog component from within a Context Menu or Dropdown Menu,
+ * you must encase the Context Menu or Dropdown Menu component in the Dialog component.
+ * For more information, refer to the linked issue [here](https://github.com/radix-ui/primitives/issues/1836).
+ */
+export const DialogWithContextMenuTrigger: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DialogWithContextMenuTriggerSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DialogWithContextMenuTriggerComponent },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DialogWithContextMenuTriggerComponent v-bind="args" />
+    `,
+  }),
+}
---
+++ .storybook/stories/ui/Dialog/FormDialog.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import type { GenericObject, SubmissionHandler } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import { h } from 'vue'
+import * as z from 'zod'
+
+import { Button } from '~/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '~/components/ui/form'
+import { Input } from '~/components/ui/input'
+import { toast } from '~/components/ui/toast'
+
+const schema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+const formSchema = toTypedSchema(schema)
+
+const onSubmit: SubmissionHandler<GenericObject> = function (values) {
+  const formValues = values as z.infer<typeof schema>
+
+  toast({
+    title: 'You submitted the following values:',
+    description: h(
+      'pre',
+      { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' },
+      h('code', { class: 'text-white' }, JSON.stringify(formValues, null, 2)),
+    ),
+  })
+}
+</script>
+
+<template>
+  <Dialog>
+    <DialogTrigger as-child>
+      <Button variant="outline">
+        Edit Profile
+      </Button>
+    </DialogTrigger>
+    <DialogContent class="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>Edit profile</DialogTitle>
+        <DialogDescription>
+          Make changes to your profile here. Click save when you're done.
+        </DialogDescription>
+      </DialogHeader>
+      <Form
+        :validation-schema="formSchema"
+        @submit="onSubmit"
+      >
+        <FormField v-slot="{ componentField }" name="username">
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="shadcn" v-bind="componentField" />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+
+        <DialogFooter>
+          <Button type="submit">
+            Save changes
+          </Button>
+        </DialogFooter>
+      </Form>
+    </DialogContent>
+  </Dialog>
+</template>
