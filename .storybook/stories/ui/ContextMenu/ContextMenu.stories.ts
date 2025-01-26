c3bd9639e01fdbdda333dfba31dd8f45c844c0b8
Achmad F. Ibrahim <acfatah@gmail.com>
2025-01-26 22:57:40 +0800

    feat(storybook): Add ContextMenu component stories

---
+++ .storybook/stories/ui/ContextMenu/ContextMenu.stories.ts
@@ -0,0 +1,70 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuGroup,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuPortal,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from '~/components/ui/context-menu'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * Displays a menu to the user — such as a set of actions or functions — triggered by a button.
+ *
+ * Primitive API Reference: https://www.radix-vue.com/components/context-menu.html
+ */
+export default {
+  title: 'Overlays/ContextMenu',
+  component: DefaultStory,
+  subcomponents: {
+    ContextMenu,
+    ContextMenuCheckboxItem,
+    ContextMenuContent,
+    ContextMenuGroup,
+    ContextMenuItem,
+    ContextMenuLabel,
+    ContextMenuPortal,
+    ContextMenuRadioGroup,
+    ContextMenuRadioItem,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
